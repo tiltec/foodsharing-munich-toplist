5c3abc63844561bb7cb4581414606018f1d249c8
Tilmann Becker <tilmann.becker@gmail.com>
2015-11-01 16:28:21 +0100

    flask json API and first database

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -2,7 +2,7 @@
 
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+     abort, render_template, flash, jsonify
      
 DATABASE = '../scraper/foodsharingstats.db'
 DEBUG = True
@@ -31,7 +31,20 @@ def teardown_request(exception):
 def show_entries():
     cur = g.db.execute('select id,date,fetchcount,fetchweight,postcount,friends from munichstats order by date desc')
     entries = [dict(userid=row[0], date=row[1], fetchcount=row[2], fetchweight=row[3], postcount=row[4], friends=row[5]) for row in cur.fetchall()]
-    return render_template('show_entries.html', entries=entries)
+    return render_template('show_entries.html', entries=entries) 
+    
+@app.route('/overview', methods=['GET'])
+def overview():
+    cur = g.db.execute('select id,date,fetchcount,fetchweight,postcount,friends from munichstats order by date desc')
+    entries = [dict(userid=row[0], date=row[1], fetchcount=row[2], fetchweight=row[3], postcount=row[4], friends=row[5]) for row in cur.fetchall()]
+    userentries = []
+    userids = set([_['userid'] for _ in entries])
+    for u in userids:
+        userentries.append(
+            {'userid': u,
+             'entries': [e for e in entries if e['userid'] == u]
+             })
+    return jsonify({'entries': userentries})
     
 if __name__ == '__main__':
     app.run()
