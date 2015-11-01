e91baefc7f191df41a3c89cd351a3c74e45113e1
Xbaal <becker.konrad@gmail.com>
2015-11-01 18:23:40 +0100

    first plot

--- frontend/main.js
+++ frontend/main.js
@@ -5,60 +5,29 @@ myapp.controller("OverviewCtrl",function($scope,$http){
         var User = resp.data.entries;
         console.log(User);
         $scope.data = User.slice(0,10);
+        $scope.highchartsNG.series = [{name:"Foodsaver Münchens",data:User.map(function (el) {
+            return el.entries[0].fetchcount;
+        })}]
     },function(resp){
         console.log("Error retrieving overview data")
     })
-})
-
-myapp.controller('myctrl', function ($scope) {
-
-    $scope.addPoints = function () {
-        var seriesArray = $scope.highchartsNG.series
-        var rndIdx = Math.floor(Math.random() * seriesArray.length);
-        seriesArray[rndIdx].data = seriesArray[rndIdx].data.concat([1, 10, 20])
-    };
-
-    $scope.addSeries = function () {
-        var rnd = []
-        for (var i = 0; i < 10; i++) {
-            rnd.push(Math.floor(Math.random() * 20) + 1)
-        }
-        $scope.highchartsNG.series.push({
-            data: rnd
-        })
-    }
-
-    $scope.removeRandomSeries = function () {
-        var seriesArray = $scope.highchartsNG.series
-        var rndIdx = Math.floor(Math.random() * seriesArray.length);
-        seriesArray.splice(rndIdx, 1)
-    }
-
-    $scope.options = {
-        type: 'line'
-    }
-
-    $scope.swapChartType = function () {
-        if (this.highchartsNG.options.chart.type === 'line') {
-            this.highchartsNG.options.chart.type = 'bar'
-        } else {
-            this.highchartsNG.options.chart.type = 'line'
-        }
-    }
 
     $scope.highchartsNG = {
         options: {
             chart: {
-                type: 'line'
+                type: 'scatter'
             }
         },
-        series: [{
-            data: [10, 15, 12, 8, 7]
-        }],
+        series: [],
         title: {
-            text: 'Hello'
+            text: 'Verteilung der aktuellen Abholungszahlen'
         },
-        loading: false
+        loading: false,
+        yAxis: {
+            title: {
+                text: 'Anzahl der Abholungen'
+            }
+        }
     }
 
 });
