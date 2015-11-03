f0d7778242c699da19887507d070cb3d2f723dbb
Tilmann Becker <tilmann.becker@gmail.com>
2015-11-03 15:11:59 +0100

    moved frontend files to flaskr/static

--- flaskr/static/main.js
+++ flaskr/static/main.js
@@ -1,7 +1,7 @@
 var myapp = angular.module("munich-toplist",["highcharts-ng"]);
 
 myapp.controller("OverviewCtrl",function($scope,$http){
-    $http.get("http://localhost:5000/overview").then(function(resp){
+    $http.get("/overview").then(function(resp){
         var User = resp.data.entries;
         console.log(User);
         $scope.data = User.slice(0,10);
@@ -15,6 +15,26 @@ myapp.controller("OverviewCtrl",function($scope,$http){
         }).filter(function (el) {
             return el[1];
         });
+
+        function parseTime(tstring) { return new Date(tstring.replace(' ','T')) }
+        function getEntriesInDateRange(all, msrange) { 
+            var recent = parseTime(all[0].date);
+            return all.filter(function(el) {
+                    return recent-parseTime(el.date) < msrange;
+                });
+        }
+        $scope.mostactivelist = User.map(function (el) {
+            var entries = getEntriesInDateRange(el.entries, 1000*60*60*24*7.1);
+            return {count: entries[0].fetchcount-entries.slice(-1)[0].fetchcount,
+                    userid: el.userid};
+        }).sort(function(a,b){return b.count-a.count}).slice(0,20);
+        
+        $scope.friendsgained = User.map(function (el) {
+            var entries = getEntriesInDateRange(el.entries, 1000*60*60*24*7.1);
+            return {count: entries[0].friends-entries.slice(-1)[0].friends,
+                    userid: el.userid};
+        }).sort(function(a,b){return b.count-a.count}).slice(0,10);
+
         var quantil_80;
         fetchcount_sum *= 0.8;
         fetchcount_data.some(function (c,i) {
@@ -25,8 +45,8 @@ myapp.controller("OverviewCtrl",function($scope,$http){
             }
         })
         $scope.quantil_80 = quantil_80;
-        $scope.highchartsNG.series = [{name:"Foodsaver Münchens",data:fetchcount_data}];
-        $scope.highchartsNG2.series = [{name:"Foodsaver Münchens",data:scatter_data,point: {
+        $scope.highchartsNG.series = [{name:"Foodsaver MÃ¼nchens",data:fetchcount_data}];
+        $scope.highchartsNG2.series = [{name:"Foodsaver MÃ¼nchens",data:scatter_data,point: {
             events: {
                 click: function (ev) {
                     var win = window.open("https://foodsharing.de/profile/"+ev.currentTarget.x, '_blank');
